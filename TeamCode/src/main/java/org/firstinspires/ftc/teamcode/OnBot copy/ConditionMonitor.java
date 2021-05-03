942f302d25f4f496e38e600661aba29a0d6972af
alanstvn <61133920+alanstvn@users.noreply.github.com>
2021-05-03 13:36:26 -0500

    Import OnBot Java Code
    Bring in code from OnBot developement

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OnBot copy/ConditionMonitor.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Condition monitor that enables monitoring additional condition while performing an
+ * operation. The operation ensures that it can be continued only if this condition
+ * permits it.
+ *
+ * @author Harshal Bharatia
+ */
+public interface ConditionMonitor
+{
+    /**
+     * Check if the current condition allows the operation to continue.
+     *
+     * @return  True if the current condition allows the operation to continue, false if
+     *   the current condition requires operation be preempted.
+     */
+    public boolean canContinue();
+}
